b653996b2eb2880d8ce19720620b4121c08ab173
leahvan-cmu <leah.vanneste@gmail.com>
2026-04-20 21:58:59 -0400

    Fixed bugs in user-login

--- Main.py
+++ Main.py
@@ -17,3 +17,6 @@ def main():
             break
         else:
             print("Invalid choice, please try again")
+
+if __name__ == '__main__':
+    main()
--- user-login.py
+++ user-login.py
@@ -1,33 +1,32 @@
-# Create login system for users to access the tool, and store the password hash in csv file to authenticate users when they log in
-
+# A login system for users to access the tool, and store the password hash in csv file to authenticate users when they log in
 
 import hashlib 
 import secrets
-import sha256 
 import csv
-salt = secrets.token_bytes(16)
 
 # takes user input of username and password, hashes the password with a salt, and stores the username and hashed password in a csv file
 def create_user():
     username = input("Create a username: ")
     password = input("Create a password: ")
-    pwd_salt = password + salt 
-    hashed_pwd = hashlib.sha256(pwd_salt.encode()).hexdigest()
+    salt = secrets.token_bytes(16)
+    pwd_bytes = salt + password.encode('utf-8') 
+    hashed_pwd = hashlib.sha256(pwd_bytes).hexdigest()
     with open('users.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([username, hashed_pwd]) 
+        writer.writerow([username, salt.hex(), hashed_pwd]) 
 
 # takes user input of username and password, hashes the password with the same salt, and checks if the hash matches the stored hash in the csv file
 def login():
     with open('users.csv', mode='r') as file:
         reader = csv.reader(file)
-        users = {rows[0]: rows[1] for rows in reader}
+        users = {rows[0]: (rows[1], rows[2]) for rows in reader}
     username = input("Username: ")
     password = input("Password: ")
-    pwd_salt = password + salt 
-    hashed_pwd = hashlib.sha256(pwd_salt.encode()).hexdigest()
+    salt = bytes.fromhex(users[username][0]) 
+    pwd_bytes = salt + password.encode('utf-8')
+    hashed_pwd = hashlib.sha256(pwd_bytes).hexdigest()
 
-    if username in users and hashed_pwd == users[username]:
+    if username in users and secrets.compare_digest(hashed_pwd, users[username][1]):
         print("Welcome back, " + username)
     else:
         print("Incorrect password, please try again")
