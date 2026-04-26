d1eda1a000105191307d1c3d0d95f052613ca390
leahvan-cmu <leah.vanneste@gmail.com>
2026-04-25 22:29:35 -0400

    Updated menu so user can access tool functions

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 # Secure File Encryption and Decryption Tool
 # Team Members: Avery Long, Leah VanNeste 
 # 4/28/2026
+from user_login import create_user, login
+from file_hashing import save_hash, verify_hash
+
 
 def main():
     print("Secure File Encryption Tool")
@@ -17,6 +20,35 @@ def main():
             break
         else:
             print("Invalid choice, please try again")
+            
+def file_menu():
+    while True:
+        print("\n1. Encrypt File")
+        print("2. Decrypt File")
+        print("3. Hash File")
+        print("4. Verify Hash")
+        print("5. Sign File")
+        print("6. Verify Signature")
+        print("7. Logout")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            encrypt_file(input("File path: "), input("Password: "))
+        elif choice == '2':
+            decrypt_file(input("File path: "), input("Password: "))
+        elif choice == '3':
+            save_hash(input("File path: "))
+        elif choice == '4':
+            verify_hash(input("File path: "))
+        elif choice == '5':
+            # when you finish Digital signature part, add the function for this here
+        elif choice == '6':
+            # when you finish Digital signature part, add the function for this here
+        elif choice == '7':
+            break
+        else:
+            print("Invalid choice, please try again")           
+               
+            
 
 if __name__ == '__main__':
     main()
